ad95e890d526be772c2a03400ffd969027cfe2df
Shafiil Ahmed Sadat <sadatshafiil@gmail.com>
2024-12-11 13:42:54 +0600

    Create ArrayOddSum.c

---
+++ ArrayOddSum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int x,sum = 0;
+    scanf("%d",&x);
+    int arr[x];
+    for(int i=0;i<x;++i)
+    {
+       scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<x;++i)
+    {
+        if(arr[i]%2!=0)
+        {
+        sum = sum + arr[i];
+        }
+    }
+
+    printf("%d",sum);
+    return 0;
+}
